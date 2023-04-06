83cd4768829c7b1a39ab90be094c5c57f84b8498
mpaulson <the.primeagen@gmail.com>
2023-04-06 13:34:53 -0600

    feat: first commit.

--- api/xor.ts
+++ api/xor.ts
@@ -1,18 +1,22 @@
 
 /* @ts-ignore */
 import wasm from '../wasm/pkg/wasm_bg.wasm?module'
-import init, { get } from '../wasm/pkg/wasm.js';
+import init, { async_test } from '../wasm/pkg/wasm.js';
 export const config = {
   runtime: 'edge',
 }
 
-
-
-
 export default async function handler(request: Request, event: Event) {
     await init(wasm);
 
-    const value = get(1);
+    const url = new URL(request.url)
+    const a = +(url.searchParams.get('a') as string);
+    try {
+        const value = await async_test(a);
+        return new Response("Value is: " + value)
+    } catch (e) {
+        console.log("E me daddy", e);
+    }
 
-  return new Response("Value is: " + value)
+    return new Response("YOU SUCK")
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,6 @@
+mod auth;
+use libsql_client::{new_client_from_config, Config, DatabaseClient, ResultSet, Row, Value};
+use url::Url;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -6,10 +9,42 @@ pub fn xor(a: u32, b: u32) -> Result<u32, JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn get(i: usize) -> Result<usize, JsError> {
-    match i {
-        69 => Ok(i),
-        _ => Err(JsError::new("Could not get number"))
-    }
-    
+pub async fn async_test(_i: usize) -> Result<String, JsError> {
+    let url: Url = "https://theprimeagen-theprimeagen.turso.io"
+        .try_into()
+        .expect("to be a url");
+
+    web_sys::console::log_1(&JsValue::from(format!("URL: {}", url)));
+
+    let client = match new_client_from_config(Config {
+        url,
+        auth_token: Some(auth::TOKEN.to_string()),
+    })
+    .await {
+        Ok(client) => client,
+        Err(e) => {
+            return Ok(format!("UNABLE TO GET CLIENT: {}", e));
+        }
+    };
+
+    let results = match client.execute("select count from counter").await {
+        Ok(results) => results,
+        Err(e) => {
+            return Ok(format!("UNABLE TO GET CLIENT: {}", e));
+        }
+    };
+
+    let items: &Value = results.rows.get(0).expect("one result").values.get(0).expect("one value");
+    let count = match items {
+        Value::Integer { value } => value,
+        _ => {
+            return Ok("UNABLE TO GET COUNT".into());
+        }
+    };
+    _ = client.execute(format!("UPDATE counter
+SET count = {}
+WHERE
+    count = {}", count + 1, count)).await;
+
+    return Ok(format!("your counter is {}", count).into());
 }
